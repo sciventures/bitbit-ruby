2b705f5a1b222220ca0932e0d99cbe78cbd310d1
Tristan Peralta <tristanperalta@gmail.com>
2016-03-10 14:53:48 +0800

    Client helper in Bitbit

--- lib/bitbit.rb
+++ lib/bitbit.rb
@@ -7,4 +7,8 @@ require 'bitbit/client'
 module Bitbit
   VERSION = '0.1.0'
   APIClientError = Class.new(StandardError)
+
+  def self.client
+    Client.new(ENV['BITBIT_CLIENT_ID'], ENV['BITBIT_CLIENT_SECRET'])
+  end
 end
